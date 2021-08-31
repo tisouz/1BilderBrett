3899c6c9b5e13ae0dd3d73348a6e010b9cb07670
tisouz <tobiasstang21@gmail.com>
2021-08-31 21:40:35 +0200

    set volume globally

--- frontend/src/containers/Content/UserPost/Post.tsx
+++ frontend/src/containers/Content/UserPost/Post.tsx
@@ -21,18 +21,45 @@ const Post = () => {
   const content = useAppSelector((state) => state.post.content);
   const loadingPost = useAppSelector((state) => state.post.loading);
   const loadingTokens = useAppSelector((state) => state.auth.loading);
-  const loading  = loadingTokens || loadingPost;
+  const loading = loadingTokens || loadingPost;
   const postId = parseInt(useParams<MatchParams>().postId);
 
   const isGif = (path: string) => {
     const fileExtension = path.split('.').pop()?.toLocaleLowerCase();
     return fileExtension === 'gif';
   }
+
+  const setVolume = () => {
+    var video: any = document.getElementById('VideoContent');
+    if (video === null)
+      return;
+
+    let storedVolume: any = localStorage.getItem('videoVolume');
+    let newVolume = 0.5;
+    if (parseFloat(storedVolume) !== null && Number.isNaN(parseFloat(storedVolume)) === false) {
+      newVolume = parseFloat(storedVolume);
+      if (newVolume < 0.0)
+        newVolume = 0;
+      else if (newVolume > 1.0)
+        newVolume = 1.0;
+    }
+    video.volume = newVolume;
+  }
+
+  const volumeChangedHandler = (event: any) => {
+    let newVolume = event.target.volume;
+    localStorage.setItem('videoVolume', newVolume.toString());
+  }
+
   useEffect(() => {
     if (tokens === null || isNaN(postId))
       return;
     dispatch(fetchPost({ accessToken: tokens.accessToken, postId: postId }));
   }, [tokens, dispatch, postId]);
+
+  useEffect(() => {
+    setVolume(); 
+  });
 
   if (isNaN(postId))
     return <Redirect to="/" />;
@@ -41,7 +68,7 @@ const Post = () => {
     return <div className='column has-text-centered'><i className="fas fa-spinner fa-spin"></i></div>;
 
 
-  let renderedContent: any = null;
+  let renderedContent: JSX.Element | null = null;
   if (content !== null) {
     switch (mediaType) {
       case 'img':
@@ -51,8 +78,8 @@ const Post = () => {
         if (isGif(content))
           renderedContent = <img src={content} alt='post content' />;
         else
-          renderedContent = <video className='VideoContent' loop autoPlay controls src={content} />;
-        break;
+          renderedContent = <video id='VideoContent' className='VideoContent' onVolumeChange={(event) => volumeChangedHandler(event) } loop autoPlay controls src={content} />;
+        break
       default:
         renderedContent = <p>unhandled content_type, lol</p>;
         break;
